df2254378b68be7ce835ef4e138ac8f4e5d53054
Oleksii Fedorov <waterlink000@gmail.com>
2015-04-18 00:37:11 +0200

    Remove unneeded comment

--- lib/money_tracking/cli/create_command.rb
+++ lib/money_tracking/cli/create_command.rb
@@ -2,7 +2,6 @@ module MoneyTracking
   module Cli
     class CreateCommand < Struct.new(:expense_factory, :store, :amount, :currency, :tags)
       def call
-        #Views::ExpenseCreated.new(id: "7dt0ibnv")
         expense_factory
           .create(raw_expense)
           .build_view(Views::ExpenseCreated)
